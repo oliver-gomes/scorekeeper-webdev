60189e2549979df52c5d90cc2a34ed0f8983015d
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-11 01:17:04 -0400

    reset game implemented

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-const setScore = document.querySelector('.input-score').value;
-const displayScore = document.querySelector('.score-display');
-const player1Btn = document.querySelector('.player-1-btn')
-const player2Btn = document.querySelector('.player-2-btn')
-const player1Display = document.querySelector('.player-1-score')
-const player2Display = document.querySelector('.player-2-score')
+const setScore = document.querySelector(".input-score").value;
+const displayScore = document.querySelector(".score-display");
+const player1Btn = document.querySelector(".player-1-btn");
+const player2Btn = document.querySelector(".player-2-btn");
+const player1Display = document.querySelector(".player-1-score");
+const player2Display = document.querySelector(".player-2-score");
+const resetButton = document.querySelector(".reset");
 let gameOver = false;
 const winningScore = 5;
 
 let p1Score = 0;
 let p2Score = 0;
 
-player1Btn.addEventListener('click', function () {
+player1Btn.addEventListener("click", function() {
   if (!gameOver) {
     p1Score++;
     if (p1Score === winningScore) {
       gameOver = true;
-      player1Display.classList.add('winner');
+      player1Display.classList.add("winner");
     }
     player1Display.textContent = p1Score;
-
   }
 });
 
-player2Btn.addEventListener('click', function () {
+player2Btn.addEventListener("click", function() {
   if (!gameOver) {
     p2Score++;
     if (p2Score === winningScore) {
       gameOver = true;
+      player2Display.classList.add("winner");
     }
     player2Display.textContent = p2Score;
   }
+});
+
+resetButton.addEventListener("click", function() {
+  p1Score = 0;
+  p2Score = 0;
+  player1Display.textContent = 0;
+  player2Display.textContent = 0;
+  player1Display.classList.remove("winner");
+  player2Display.classList.remove("winner");
+  gameOver = false;
 });
