3e4e963edb7fbe02c003bce7d9a311aa1a6d013f
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-11 00:40:51 -0400

    Grabbing the right elements for interaction

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const setScore = document.querySelector('.inputScore').value;
-const displayScore = document.querySelector('.scoreDisplay');
-const player1 = document.querySelector('.player-1')
-const player2 = document.querySelector('.player-2')
+const setScore = document.querySelector('.input-score').value;
+const displayScore = document.querySelector('.score-display');
+const player1Btn = document.querySelector('.player-1-btn')
+const player2Btn = document.querySelector('.player-2')
+const player1Display = document.querySelector('.player-1-score')
 
+let score1 = 0;
 
+player1Btn.addEventListener('click', function () {
+  player1Display.textContent = 0;
+});
