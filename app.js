c4d6be0de183b1fce10f056a21092ac8110494f3
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-11 01:03:03 -0400

    Game Over implemented for both player

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ const player1Btn = document.querySelector('.player-1-btn')
 const player2Btn = document.querySelector('.player-2-btn')
 const player1Display = document.querySelector('.player-1-score')
 const player2Display = document.querySelector('.player-2-score')
+let gameOver = false;
+const winningScore = 5;
 
 let p1Score = 0;
 let p2Score = 0;
 
 player1Btn.addEventListener('click', function () {
-  p1Score++;
-  player1Display.textContent = p1Score;
+  if (!gameOver) {
+    p1Score++;
+    if (p1Score === winningScore) {
+      gameOver = true;
+    }
+    player1Display.textContent = p1Score;
+  }
 });
 
 player2Btn.addEventListener('click', function () {
-  p2Score++;
-  player2Display.textContent = p2Score;
+  if (!gameOver) {
+    p2Score++;
+    if (p2Score === winningScore) {
+      gameOver = true;
+    }
+    player2Display.textContent = p2Score;
+  }
 });
