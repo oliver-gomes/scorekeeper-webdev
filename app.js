81bb01cf8c359aa897c39415174b60eb700a24c4
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-11 00:17:58 -0400

    All required elements setup

---
+++ app.js
@@ -0,0 +1,6 @@
+const setScore = document.querySelector('.inputScore').value;
+const displayScore = document.querySelector('.scoreDisplay');
+const player1 = document.querySelector('.player-1')
+const player2 = document.querySelector('.player-2')
+
+
