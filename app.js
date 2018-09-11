c48bac443e1293354e0910220e785d713c4cd2a8
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-11 00:46:50 -0400

    Update to display p2 score

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const setScore = document.querySelector('.input-score').value;
 const displayScore = document.querySelector('.score-display');
 const player1Btn = document.querySelector('.player-1-btn')
-const player2Btn = document.querySelector('.player-2')
+const player2Btn = document.querySelector('.player-2-btn')
 const player1Display = document.querySelector('.player-1-score')
+const player2Display = document.querySelector('.player-2-score')
 
 let p1Score = 0;
+let p2Score = 0;
 
 player1Btn.addEventListener('click', function () {
   p1Score++;
   player1Display.textContent = p1Score;
+});
+
+player2Btn.addEventListener('click', function () {
+  p2Score++;
+  player2Display.textContent = p2Score;
 });
